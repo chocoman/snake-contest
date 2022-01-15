c23637734a88054e083014f9be1e4b9b0de7b76e
Martin Spanel <martin.spanel@newtontech.cz>
2022-01-15 14:06:34 +0100

    code style improvements

--- snake.py
+++ snake.py
@@ -4,10 +4,81 @@ from random import randint, shuffle
 from datetime import *
 import os
 
-def minus_players_points(player):
-    # pro razeni. Radime sestupne podle bodu.
-    # V pripade rovnosti vzestupne podle jmena.
-    return (-player.points, player.name) 
+class Board:
+  def __init__(self, height, width):
+    self.height = height
+    self.width = width
+    self.game_ended = False
+    self.snakes = []
+    self.dead = []
+    self.food = []
+    self.create_food()
+    self.turn_number = 0
+
+  def add_snake(self, snake):
+    self.snakes.append(snake)
+
+  def create_food(self):
+    x = randint(0, self.width - 1)
+    y = randint(0, self.height - 1)
+    if (self.is_occupied(x, y)):
+      self.create_food()
+      return
+    self.food.append([x, y])
+
+  def evaluate_state(self):
+    self.game_ended = False
+    for snake in self.snakes:
+      if (snake.points < -30):
+        print('vyrazen hrac: ' + str(snake.name))
+        self.snakes.remove(snake)
+        snake.smrt()
+        self.dead.append(snake)
+      if (snake.points > 30):
+        self.game_ended = True
+    if (len(self.snakes) <= 1):
+      self.game_ended = True
+    if (self.game_ended):
+      self.write_summary()
+
+  def write_summary(self):
+    if not os.path.exists('results'):
+      os.makedirs('results')
+    file_name = 'results/result' + str(datetime.now().strftime('%Y-%m-%d_%H_%M_%S')) + '.txt'
+    with open(file_name,'w') as file:
+      file.write(file_name + '\n')
+      for snake in self.dead:
+        file.write(str(snake.name) + ': ' + str(snake.points) + '\n')
+      for snake in self.snakes:
+        file.write(str(snake.name) + ': ' + str(snake.points) + '\n')
+        print(str(snake.name) + ': ' + str(snake.points))
+      print('Konec hry. Vysledek zapsan do ' + file_name)
+
+  def update(self):
+    self.turn_number = self.turn_number + 1
+    shuffle(self.snakes) # pohyb hadu se vyhodnoti v nahodnem poradi
+    for snake in self.snakes:
+      snake.akce()
+    self.evaluate_state()
+    for snake in self.snakes:
+      if(isinstance(snake, Snake_AI)):
+        snake.update(self.snakes)
+
+
+  def is_occupied(self, x, y):
+    if x >= self.width:
+      return True
+    if x < 0:
+      return True
+    if y >= self.height:
+      return True
+    if y < 0:
+      return True
+    for snake in self.snakes:
+      for clanek in snake.tail:
+        if (clanek == [x,y]):
+          return True
+    return False
 
 class SnakeGame:
   def __init__(self, field_size, height, width, delay):
@@ -28,24 +99,21 @@ class SnakeGame:
     self.window.bind('<KeyPress>', self.key_press)
     self.window.bind('<KeyRelease>', self.key_release)
 
-    self.game_ended = False
-    self.snakes = []
-    self.dead = []
-    self.food = []
-    self.create_food()
-    self.turn_number = 0
+    self.board = Board(height, width)
+
+  def start(self):
+    self.update_loop()
+
+  def update_loop(self):
+    print('update')
+    self.board.update()
+    self.rerender()
+    if (not self.board.game_ended):
+      self.window.after(self.delay, self.update_loop)
 
   def add_snake(self, snake):
-    self.snakes.append(snake)
+    self.board.add_snake(snake)
 
-  def create_food(self):
-    x = randint(0, self.width)
-    y = randint(0, self.height)
-    if (self.is_occupied(x, y)):
-      self.create_food()
-      return
-    self.food.append([x, y])
-    
   def key_press(self, event):
     if not event.keysym in self.keys:
       self.keys.append(event.keysym)
@@ -55,65 +123,31 @@ class SnakeGame:
   def key_release(self, event):
     self.keys.remove(event.keysym)
 
-  def evaluate_state(self):
-    self.game_ended = False
-    for snake in self.snakes:
-      if (snake.points < -30):
-        print('vyrazen hrac: ' + str(snake.name))
-        self.snakes.remove(snake)
-        snake.smrt()
-        self.dead.append(snake)
-      if (snake.points > 30):
-        self.game_ended = True
-    if (len(self.snakes) <= 1):
-      self.game_ended = True
-    if (self.game_ended):
-      if not os.path.exists('vysledky'):
-        os.makedirs('vysledky')
-      file_name = 'vysledky/vysledek' + str(datetime.now().strftime('%Y-%m-%d_%H_%M_%S')) + '.txt'
-      with open(file_name,'w') as file:
-        file.write(file_name + '\n')
-        for snake in self.dead:
-          file.write(str(snake.name) + ': ' + str(snake.points) + '\n')
-        for snake in self.snakes:
-          file.write(str(snake.name) + ': ' + str(snake.points) + '\n')
-          print(str(snake.name) + ': ' + str(snake.points))
-        print('Konec hry. Vysledek zapsan do ' + file_name)
-
-  def akce(self):
-    self.turn_number = self.turn_number + 1
-    shuffle(self.snakes) # pohyb hadu se vyhodnoti v nahodnem poradi
-    for snake in self.snakes:
-      snake.akce()
-    self.evaluate_state()
-    self.rerender()
-    for snake in self.snakes:
-      if(isinstance(snake, player_AI)):
-        snake.update(self.snakes)
-
-    if (not self.game_ended):
-      self.window.after(self.delay, self.akce)
-
   def rerender(self):
     self.canvas.delete(ALL)
-    for snake in self.snakes:
+    for snake in self.board.snakes:
       self.render_snake(snake)
     self.render_food()
     self.render_state()
 
   def render_food(self):
-    for coordinates in self.food:
-      x = coordinates[0]
-      y = coordinates[1]
+    for coordinates in self.board.food:
+      x, y = coordinates
       field_size = self.field_size
-      self.canvas.create_rectangle(x * field_size, y * field_size,
-                                   (x+1) * field_size, (y + 1) * field_size,
-                                   fill='#eeee88')
+      self.canvas.create_rectangle(
+        x * field_size,
+        y * field_size,
+        (x+1) * field_size, (y + 1) * field_size,
+        fill='#eeee88',
+      )
 
   def render_state(self):
-    self.snakes.sort(key = minus_players_points)
-    for i in range(len(self.snakes)):
-      snake = self.snakes[i]
+    sorted_snakes = sorted(
+      self.board.snakes,
+      key = lambda snake: -snake.points, # sort decreasing by points
+    )
+    for i in range(len(sorted_snakes)):
+      snake = sorted_snakes[i]
       self.canvas.create_text(
         20,
         (i + 1) * self.field_size,
@@ -132,24 +166,10 @@ class SnakeGame:
         fill = snake.color,
       )
 
-  def is_occupied(self, x, y):
-    if x >= self.width:
-      return True
-    if x < 0:
-      return True
-    if y >= self.height:
-      return True
-    if y < 0:
-      return True
-    for snake in self.snakes:
-      for clanek in snake.tail:
-        if (clanek == [x,y]):
-          return True
-    return False
 
 class Snake(object):
-  def __init__(self, app, length, color, name, klavesy_pohyb):
-    self.a = app
+  def __init__(self, board, length, color, name, klavesy_pohyb):
+    self.board = board
     self.length = length
     self.color=color
     self.name=name
@@ -163,22 +183,22 @@ class Snake(object):
     self.revive()
 
   def move(self,smer):
-    if smer == 0 and self.x < self.a.width:        # doprava
+    if smer == 0 and self.x < self.board.width:        # doprava
       self.x = self.x + 1
     elif smer == 1 and self.y > 0:                 # nahoru
       self.y = self.y - 1
     elif smer == 2 and self.x > 0:                 # doleva
       self.x = self.x - 1
-    elif smer == 3 and self.y < self.a.height:      # dolu
+    elif smer == 3 and self.y < self.board.height:      # dolu
       self.y = self.y + 1
     else:
       self.collision()
       return
-    if (self.a.is_occupied(self.x,self.y)):
+    if (self.board.is_occupied(self.x,self.y)):
       self.collision()
     self.tail.append([self.x,self.y])
-    for i in range(len(self.a.food)):
-      clanek = self.a.food[i]
+    for i in range(len(self.board.food)):
+      clanek = self.board.food[i]
       if (clanek == [self.x,self.y]):
         self.food_eaten(i)
         return
@@ -188,12 +208,12 @@ class Snake(object):
   def food_eaten(self,i):
     self.length = self.length + 3
     self.points = self.points + 1
-    self.a.food.pop(i)
-    self.a.create_food()
+    self.board.food.pop(i)
+    self.board.create_food()
   
   def revive(self):
-    self.x=randint(0, self.a.width)
-    self.y=randint(0, self.a.height)
+    self.x=randint(0, self.board.width)
+    self.y=randint(0, self.board.height)
     self.smer=randint(0, 3)
     self.tail = [[self.x, self.y]]
     self.length = 5
@@ -208,11 +228,11 @@ class Snake(object):
     self.move(self.smer)
   
   def smrt(self):
-    self.time_of_death = self.a.turn_number
+    self.time_of_death = self.board.turn_number
 
 class Snake_AI(Snake):
-  def __init__(self,aplikace, length, color, name, klavesy, ai):
-    Snake.__init__(self,aplikace, length, color, name, klavesy)
+  def __init__(self, board, length, color, name, klavesy, ai):
+    Snake.__init__(self, board, length, color, name, klavesy)
     self.ai = ai
 
   def zmena_smeru(self, klavesy):
--- startsnake.py
+++ startsnake.py
@@ -1,16 +1,15 @@
-from snake import SnakeGame, player_AI
+from snake import SnakeGame, Snake_AI
 from random import randint
 from Spanel import *
 from tkinter import mainloop
 
 a = SnakeGame(20,20,30,100)
-h1 = player_AI(a, 5, "#0000ff","h1",["Right","Up","Left","Down"],ai_Spanel_random1(a))
+h1 = Snake_AI(a.board, 5, "#0000ff","h1",["Right","Up","Left","Down"],ai_Spanel_random2(a.board))
 #h1 = snake.hrac(a, 5, "#bbff00","h2",["d","w","a","s"])
-h2 = player_AI(a, 5, "#ff0000","h2",["d","w","a","s"],ai_Spanel_random2(a))
+h2 = Snake_AI(a.board, 5, "#ff0000","h2",["d","w","a","s"],ai_Spanel_random2(a.board))
 a.add_snake(h1)
 a.add_snake(h2)
 print("zacatek hry")
-a.akce()
+a.start()
 mainloop()
-
 
