14319567a0f125c32e17f6a5245d50ea92ff166c
Martin Spanel <martin.spanel@newtontech.cz>
2022-02-22 13:29:56 +0100

    implemented explorer score

--- participants/martin.py
+++ participants/martin.py
@@ -1,4 +1,5 @@
 from direction import LEFT, UP, RIGHT, DOWN, direction_as_string
+from random import randint
 
 def update_coordinates(row, col, direction):
   if direction == LEFT: col -= 1
@@ -8,10 +9,11 @@ def update_coordinates(row, col, direction):
   return row, col
 
 class AI:
-  def __init__(self):
+  def __init__(self, search_steps=20):
     self.fields = [['']]
     self.height = 1
     self.width = 1
+    self.search_steps=search_steps
 
   def is_free(self, row, col, direction=None):
     row, col = update_coordinates(row, col, direction)
@@ -32,8 +34,6 @@ class AI:
     self.head_r, self.head_c = 0, 0
     self.food_r, self.food_c = 0, 0
     self.opponent_heads = []
-    opponent_names = []
-    snake_names = []
     for i in range(self.height):
       for j in range(self.width):
         if fields[i][j] == 'f':
@@ -41,15 +41,35 @@ class AI:
         if fields[i][j] == 'h':
           self.head_r, self.head_c = i, j
         if fields[i][j].startswith('0-'):
-          name = fields[i][j][2:]
-          opponent_names.append(name)
           self.opponent_heads.append([i,j])
-        if fields[i][j].startswith('1-'):
-          name = fields[i][j][2:]
-          snake_names.append(name)
-    for name in snake_names:
-      if name not in opponent_names:
-        self.my_name = name
+    for opponent_head_row, opponent_head_col in self.opponent_heads:
+      for direction in [UP, LEFT, RIGHT, DOWN]:
+        danger_row, danger_col = update_coordinates(opponent_head_row, opponent_head_col, direction)
+        if (self.is_free(danger_row, danger_col, direction)):
+          fields[danger_row][danger_col] = 'd'
+
+
+  def explorer_score(self, direction):
+    row, col = update_coordinates(self.head_r, self.head_c, direction)
+    previous_steps = [[row, col]]
+    distance = 0
+    search_depth = 20
+    for i in range(search_depth):
+      safe_directions = []
+      for direction in [UP, LEFT, RIGHT, DOWN]:
+        next_coordinates = update_coordinates(row, col, direction)
+        if self.is_free(row, col, direction) and next_coordinates not in previous_steps:
+          safe_directions.append(direction)
+      if len(safe_directions) == 0:
+        break
+      random_direction = safe_directions[randint(0, len(safe_directions) - 1)]
+      row, col = update_coordinates(row, col, random_direction)
+      previous_steps.append((row, col))
+      distance += 1
+    if distance == search_depth:
+      distance += 40 # bonus for reaching search depth - we assume that there are more free spaces
+    return distance
+
 
   def snake_distance_score(self, direction):
     row, col = update_coordinates(self.head_r, self.head_c, direction)
@@ -59,64 +79,24 @@ class AI:
       distance = abs(row - opponent_r) + abs(col - opponent_c)
       if distance < closest_head_distance:
         closest_head_distance = distance
-    print(f'closest snake: {closest_head_distance}')
-    return closest_head_distance
+    return closest_head_distance ** 0.5
 
   def food_distance_score(self, direction):
     row, col = update_coordinates(self.head_r, self.head_c, direction)
     distance = abs(row - self.food_r) + abs(col - self.food_c)
     return 1 / (distance + 1) ** 0.2
 
-  def wall_distance_score(self, direction):
-    row, col = update_coordinates(self.head_r, self.head_c, direction)
-    if not self.is_free(row, col):
-      return -1000000
-    closest_wall_distance = min(
-      row,
-      col,
-      self.width - col,
-      self.height - row,
-    )
-    for i in range(self.height):
-      for j in range(self.width):
-        if self.is_free(i, j):
-          continue # skip fields that are not walls
-        if (self.fields[i][j] == 'h'):
-          continue
-        distance = abs(row - i) + abs(col - j)
-        if (self.fields[i][j].endswith('-' + self.my_name)):
-          piece_number = int(self.fields[i][j][:-len(self.my_name)-1])
-          if piece_number <= distance + 1:
-            # don't count pieces from my own neck.
-            # If distant parts of my neck are close to my head, they count
-            continue 
-        if distance < closest_wall_distance:
-          closest_wall_distance = distance
-    return closest_wall_distance
-
   def score_direction(self, direction):
     if not self.is_free(self.head_r, self.head_c, direction):
       return -float('inf')
-    opponents_count = len(self.opponent_heads)
     score = (
-      opponents_count * self.snake_distance_score(direction) +
-      (opponents_count + 0.1) * self.wall_distance_score(direction) +
-      50 * self.food_distance_score(direction)
+      5 * self.snake_distance_score(direction) +
+      400 * self.food_distance_score(direction) +
+      sum([self.explorer_score(direction) for i in range(self.search_steps)]) / self.search_steps
     )
     print(direction_as_string(direction), score)
     return score
 
-
-
-  # Funkce dostane seznam seznamů stringů fields. Záznam fields[i][j] říká,
-  # co je na políčku na řádku i ve sloupci j. Možné hodoty jsou 'f', což značí jídlo,
-  # prázdný string '' což značí prázdné pole, 'h' značí hlavu hada, pro nějž se rozhodujete
-  # a cokoli dalšího znamená tělo nějakého hada. To je záznam ve formátu '4-arnost'.
-  # Číslo před pomlčkou značí, o kolikátý díl těla se jedná (hlava je 0) a zbytek za pomlčkou
-  # je jméno daného hada.
-  # Tuto funkci můžete libovolně upravit. Jenom zachovejte její název a vstupní parametry
-  # a vracejte jedině jednu z hodnot LEFT, UP, RIGHT, DOWN
-  # Můžete přidat jakékoli další funkce.
   def decide_direction(self, fields):
     self.update(fields)
     best_score = -1000000
